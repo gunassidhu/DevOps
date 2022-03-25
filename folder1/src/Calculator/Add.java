ac3a7f8ba525cacac01056a138568e6668aa17d7
Gunasagar Pullamchetty <gsagarsidhu@gmail.com>
2022-03-25 22:26:12 +0530

    Second Commit

---
+++ folder1/src/Calculator/Add.java
@@ -0,0 +1,15 @@
+package Calculator;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0;
+		int y=0;
+		int z;
+		z= x+y;
+		System.out.print(z);
+
+	}
+
+}
